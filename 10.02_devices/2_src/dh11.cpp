fadf947c7c4ceaadcf0d45a6ef787e4b1c7a8c83
Michael Robinson <kb1dds@gmail.com>
2023-08-20 22:34:56 -0400

    Starting to add real DH11 functionality...

--- 10.02_devices/2_src/dh11.cpp
+++ 10.02_devices/2_src/dh11.cpp
@@ -1,7 +1,7 @@
 /* dh11.hpp: DH11 asynchronous serial line interface
 
+ Copyright (c) 2023, Michael Robinson
  Copyright (c) 2018, Joerg Hoppe
- j_hoppe@t-online.de, www.retrocmp.com
 
  Permission is hereby granted, free of charge, to any person obtaining a
  copy of this software and associated documentation files (the "Software"),
@@ -16,17 +16,17 @@
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- JOERG HOPPE BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ JOERG HOPPE NOR MICHAEL ROBINSON BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ IN THE SOFTWARE.
 
+ 20-aug-2023  MR      Starting to emulate correct device behavior
  19-oct-2022  MR      Copied over from mit_ng device
  12-nov-2018  JH      entered beta phase
 
  DH11:
- The DH11 asynchronous serial line interface is a UNIBUS peripheral which provides up to 16 asynchronous serial lines. 
- This Unibone device does just one line
- Currently these are just there as stubs...
+ The DH11 asynchronous serial line interface is a UNIBUS peripheral which provides up to 16 asynchronous serial lines.
  No active register callbacks, just polling in worker()
  */
 
@@ -46,67 +46,78 @@ dh11_c::dh11_c() :
 	type_name.value = "dh11_c";
 	log_label = "dh11";
 
-	set_default_bus_params(0760020, 31, 0340, 5); // base addr, priority slot, intr-vector, intr level
+	set_default_bus_params(DH11_ADDR, DH11_SLOT, DH11_VECTOR, DH11_LEVEL);
 
-	// controller has 8*16=128 registers for 16 lines.  That might be too many for Unibone at the moment...
-        // so this device just does one line
+	// controller has 8 programmer-visible registers, though maintains other per-line registers
 	register_count = 8;
 
 	scr_reg = &(this->registers[0]); // @  base addr
-	strcpy(scr_reg->name, "DHSCR"); // "CSR"
+	strcpy(scr_reg->name, "DHSCR"); // System control register (selects line and other parameters that apply to all lines)
 	scr_reg->active_on_dati = false; // no controller state change
 	scr_reg->active_on_dato = false;
 	scr_reg->reset_value = 0;
-	scr_reg->writable_bits = 0xffff; // not sure ... TBD
+	scr_reg->writable_bits = 0xffff; // all bits writable
 
 	nrcr_reg = &(this->registers[1]); // @  base addr+2
-	strcpy(nrcr_reg->name, "DHNRCR"); 
+	strcpy(nrcr_reg->name, "DHNRCR"); // Next character received (bottom of silo)
 	nrcr_reg->active_on_dati = false; // no controller state change
 	nrcr_reg->active_on_dato = false;
 	nrcr_reg->reset_value = 0;
-	nrcr_reg->writable_bits = 0xffff; // not sure ... TBD
+	nrcr_reg->writable_bits = 0x0000; // read only
 
 	lpr_reg = &(this->registers[2]); // @  base addr+4
-	strcpy(lpr_reg->name, "DHLPR"); 
+	strcpy(lpr_reg->name, "DHLPR");  // Line parameter register (mirrors a per-line register)
 	lpr_reg->active_on_dati = false; // no controller state change
 	lpr_reg->active_on_dato = false;
 	lpr_reg->reset_value = 0;
-	lpr_reg->writable_bits = 0xffff; // not sure ... TBD
+	lpr_reg->writable_bits = 0xffff; // Write-only, in fact
 
 	car_reg = &(this->registers[3]); // @  base addr+6
-	strcpy(car_reg->name, "DHCAR"); 
+	strcpy(car_reg->name, "DHCAR");  // Current address register (mirrors a per-line register)
 	car_reg->active_on_dati = false; // no controller state change
 	car_reg->active_on_dato = false;
 	car_reg->reset_value = 0;
-	car_reg->writable_bits = 0xffff; // not sure ... TBD
+	car_reg->writable_bits = 0xffff; // Write-only, in fact
 
 	bcr_reg = &(this->registers[4]); // @  base addr+8
-	strcpy(bcr_reg->name, "DHBCR"); 
+	strcpy(bcr_reg->name, "DHBCR");  // Byte count register (mirrors a per-line register)
 	bcr_reg->active_on_dati = false; // no controller state change
 	bcr_reg->active_on_dato = false;
 	bcr_reg->reset_value = 0;
-	bcr_reg->writable_bits = 0xffff; // not sure ... TBD
+	bcr_reg->writable_bits = 0xffff; // read/write
 
 	bar_reg = &(this->registers[5]); // @  base addr+10
-	strcpy(bar_reg->name, "DHBAR"); 
+	strcpy(bar_reg->name, "DHBAR");  // Buffer active register (applies to all lines, one bit per line)
 	bar_reg->active_on_dati = false; // no controller state change
 	bar_reg->active_on_dato = false;
 	bar_reg->reset_value = 0;
-	bar_reg->writable_bits = 0xffff; // not sure ... TBD
+	bar_reg->writable_bits = 0xffff; // Read/write
 
 	brcr_reg = &(this->registers[6]); // @  base addr+12
-	strcpy(brcr_reg->name, "DHBRCR"); 
+	strcpy(brcr_reg->name, "DHBRCR"); // Break control register (applies to all lines, one bit per line)
 	brcr_reg->active_on_dati = false; // no controller state change
 	brcr_reg->active_on_dato = false;
 	brcr_reg->reset_value = 0;
-	brcr_reg->writable_bits = 0xffff; // not sure ... TBD
+	brcr_reg->writable_bits = 0xffff; // Write-only, in fact
 
 	ssr_reg = &(this->registers[7]); // @  base addr+14
-	strcpy(ssr_reg->name, "DHSSR"); 
+	strcpy(ssr_reg->name, "DHSSR");  // Silo status register (applies to all lines)
 	ssr_reg->active_on_dati = false; // no controller state change
 	ssr_reg->active_on_dato = false;
 	ssr_reg->reset_value = 0;
-	ssr_reg->writable_bits = 0xffff; // not sure ... TBD
+	ssr_reg->writable_bits = 0xffff; // read/write
+
+	// Initialize the silo
+	this.silo_count = 0;
+	this.silo_alarm = 0;
+
+	// Clear per-line registers
+	for (int i = 0; i < 16; i ++ ) {
+	  this.lpr_line_reg[i] = 0;
+	  this.car_line_reg[i] = 0;
+	  this.bcr_line_reg[i] = 0;
+	}     
+
 }
 
 dh11_c::~dh11_c() {
@@ -152,5 +163,79 @@ void dh11_c::on_init_changed(void) {
 		reset_unibus_registers();
 		INFO("dh11_c::on_init()");
 	}
+
+	// Initialize the silo
+	this.silo_count = 0;
+	this.silo_alarm = 0;
+
+	// Clear per-line lpr registers.  (Per EK-0DH11-MM-003_Apr75, paragraph 3.3.7 per-line CAR and BCR are not touched during initialize)
+	for (int i = 0; i < 16; i ++ ) {
+	  this.lpr_line_reg[i] = 0;
+	}     
 }
 
+
+// Add a character to the top of the silo
+// Inputs: incoming : incoming character (lowest 8 bits only)
+//         line     : line that received the character (lowest 4 bits only)
+//         parity_error, framing_error, data_overrun : various kinds of error conditions from UART
+// Outputs: character stored on silo
+// Returns: true if success, false if silo is full
+// Fires interrupts according to
+//  * If silo alarm level is tripped
+//  * If storage interrupt is tripped (silo is already full)
+// Note: in case of silo full, character is *not* placed into the silo; it's left in limbo
+bool dh11_c::silo_enqueue( uint8_t incoming, uint8_t line, bool parity_error, bool framing_error, bool data_overrun ) {
+
+  if( this.silo_count > 63 ){
+    uint16_t val = get_register_dato_value(scr_reg);
+    set_register_dati_value(ssr_reg, val | SCR_STORAGE_INT ,  __func__);
+
+    // Don't do anything to the silo, 'cause it's full!
+    
+    if (get_register_dato_value(scr) & SCR_S_INT_ENABLE) {
+      
+      // Trigger storage interrupt
+      
+    }
+    return false;
+  }
+
+  // Add the data to the silo, including the data valid flag
+  this.silo[this.silo_count] = (incoming & 0x00ff) | ((line & 0x000f) << 8) |
+    (parity_error & NCR_PARITY_ERR) | (framing_error & NCR_FRAMING_ERR) | (data_overrun & NCR_DATA_OVERRUN) | NCR_VALID_DATA;
+  this.silo_count ++;
+
+  uint16_t val = get_register_dato_value(ssr_reg);
+
+  // Update SSR with new silo fill level
+  set_register_dati_value(ssr_reg, (val & (~SSR_SILO_FILL)) | ((this.silo_count & 0x003f) << 8) , __func__);
+
+  int silo_alarm_level = val & SSR_SILO_ALARM;
+
+  if( (this.silo_count > silo_alarm_level) & (get_register_dato_value(scr) & SCR_RX_INT_ENABLE) ){
+    // Trigger silo alarm interrupt
+    return true;
+  }
+  return true
+}
+
+// Remove a character (it's actually a word) from the bottom of the silo.  NB: If valid data present, bit 15 is set.  If the silo is empty, bit 15 is cleared
+void dh11_c::silo_dequeue( uint16_t *data ) {
+  if( this.silo_count == 0 ){
+    *data = this.silo[0] & (~NCR_VALID_DATA);
+  }
+  else {
+    *data = this.silo[0] | NCR_VALID_DATA;
+
+    for( int i = 1; i < this.silo_count; i ++ ) {
+      this.silo[i-1] = this.silo[i];
+    }
+    this.silo_count --;
+  }
+
+  uint16_t val = get_register_dato_value(ssr_reg);
+
+  // Update SSR with new silo fill level
+  set_register_dati_value(ssr_reg, (val & (~SSR_SILO_FILL)) | ((this.silo_count & 0x003f) << 8) , __func__);
+}
--- 10.02_devices/2_src/dh11.hpp
+++ 10.02_devices/2_src/dh11.hpp
@@ -1,7 +1,7 @@
 /* dh11.hpp: DH11 asynchronous serial line interface
 
+ Copyright (c) 2023, Michael Robinson
  Copyright (c) 2018, Joerg Hoppe
- j_hoppe@t-online.de, www.retrocmp.com
 
  Permission is hereby granted, free of charge, to any person obtaining a
  copy of this software and associated documentation files (the "Software"),
@@ -16,11 +16,12 @@
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- JOERG HOPPE BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ JOERG HOPPE NOR MICHAEL ROBINSON BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ IN THE SOFTWARE.
 
-
+ 20-aug-2023  MR      Starting to emulate correct device behavior
  19-oct-2022  MR      Copied over from dh11 device
  12-nov-2018  JH      entered beta phase
  */
@@ -34,19 +35,76 @@ using namespace std;
 #include "utils.hpp"
 #include "qunibusdevice.hpp"
 
+// Overall device definitions
+#define DH11_ADDR       0760020
+#define DH11_SLOT       31
+#define DH11_LEVEL	05
+#define DH11_VECTOR	0340	
+
+// register bit definitions
+// SCR
+#define SCR_RX_INT_ENABLE  0x0040
+#define SCR_RX_INT         0x0080
+#define SCR_CLR_NXM        0x0100
+#define SCR_MAINT          0x0200
+#define SCR_NXM            0x0400
+#define SCR_MASTER_CLR     0x0800
+#define SCR_S_INT_ENABLE   0x1000
+#define SCR_NXM_INT_ENABLE 0x2000
+#define SCR_STORAGE_INT    0x4000
+#define SCR_TX_INT         0x8000
+
+// NCRCR
+#define NCR_PARITY_ERR   0x1000
+#define NCR_FRAMING_ERR  0x2000
+#define NCR_DATA_OVERRUN 0x4000
+#define NCR_VALID_DATA   0x8000
+
+// LPR
+#define LPR_CHARLEN       0x0003
+#define LPR_STOP_BITS     0x0004
+#define LPR_PARITY_ENABLE 0x0010
+#define LPR_ODD_PARITY    0x0020
+#define LPR_RX_SPEED      0x03c0
+#define LPR_TX_SPEED      0x3c00
+#define LPR_DUPLEX        0x4000
+#define LPR_AUTO_ECHO     0x8000
+
+// SSR
+#define SSR_SILO_ALARM 0x003f
+#define SSR_READ_XM    0x00c0
+#define SSR_SILO_FILL  0x3f00
+#define SSR_MAINT      0x8000
+
 class dh11_c: public qunibusdevice_c {
 private:
 
-	// One of each of the following for each line
-        qunibusdevice_register_t *scr_reg; // System control register
+	// Programmer-visible registers 
+        qunibusdevice_register_t *scr_reg;  // System control register
         qunibusdevice_register_t *nrcr_reg; // Next recieved character register
-        qunibusdevice_register_t *lpr_reg; // Line parameter register
-        qunibusdevice_register_t *car_reg; // Current address register
-        qunibusdevice_register_t *bcr_reg; // Byte count register
-        qunibusdevice_register_t *bar_reg; // Buffer active register
+        qunibusdevice_register_t *lpr_reg;  // Line parameter register
+        qunibusdevice_register_t *car_reg;  // Current address register
+        qunibusdevice_register_t *bcr_reg;  // Byte count register
+        qunibusdevice_register_t *bar_reg;  // Buffer active register
         qunibusdevice_register_t *brcr_reg; // Break control register
-        qunibusdevice_register_t *ssr_reg; // Silo status register
+        qunibusdevice_register_t *ssr_reg;  // Silo status register
 
+        // Silo contents and top (shared across all lines for incoming data)
+        uint16_t silo_count;  // Zero means that the silo is empty, so this is effectively the *next* silo location.
+                                  // Also, the seventh bit gets placed into bit 14 of SCR
+        uint16_t silo[64];
+
+        // Per-line registers (not programmer-visible)
+        uint16_t lpr_line_reg[16];
+        uint16_t car_line_reg[16];
+        uint16_t bcr_line_reg[16];
+
+        // Add a character to the top of the silo
+        bool silo_enqueue( uint8_t incoming, uint8_t line, bool parity_error, bool framing_error, bool data_overrun ); 
+
+        // Remove a character (it's actually a word) from the bottom of the silo.  NB: If valid data present, bit 15 is set.  If the silo is empty, bit 15 is cleared
+        void silo_dequeue( uint16_t *data );
+  
 public:
 
 	dh11_c();
